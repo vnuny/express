ad1a19233951fd3ea3860a062af25420f5ec5edd
vnun <vnun.b2b@gmail.com>
2025-03-18 01:25:20 +0300

    fix

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const axios = require("axios");
 let port = 3001;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello World🫦"
-  });
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://cdn32.weecimaa.cfd/");
+    console.log(response.data);
+    res.send(response.data);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
